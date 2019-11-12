f2b2bceeb9887a892a2f7dfe896aa4587d3388bd
bodhibrdr <julie.gumerman@gmail.com>
2019-11-12 14:44:32 -0700

    switches on filters screen. yay

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,13 +1,55 @@
-import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { Text, View, StyleSheet, Switch } from "react-native";
 
 import {HeaderButtons, Item} from "react-navigation-header-buttons";
 
+import Colors from "../constants/Colors";
 import HeaderButton from "../components/HeaderButton";
 const FiltersScreen = props => {
+    const [isGlutenFree, setIsGlutenFree] = useState(false);
+    const [isVegan, setIsVegan] = useState(false);
+    const [isVegetarian, setIsVegetarian] = useState(false);
+    const [isLactoseFree, setIsLactoseFree] = useState(false);
+
     return (
         <View style={styles.screen}>
-            <Text>The filters screen</Text>
+            <Text style={styles.title}>Available Filters</Text>
+            <View style={styles.filterContainer}>
+                <Text>Gluten-Free</Text>
+                <Switch 
+                    thumbColor={Colors.accentColor}
+                    trackColor={{true: Colors.primaryColor}}
+                    value={isGlutenFree} 
+                    onValueChange={newValue => setIsGlutenFree(newValue)}
+                />
+            </View>
+            <View style={styles.filterContainer}>
+                <Text>Vegan</Text>
+                <Switch 
+                    thumbColor={Colors.accentColor}
+                    trackColor={{true: Colors.primaryColor}}
+                    value={isVegan} 
+                    onValueChange={newValue => setIsVegan(newValue)}
+                />                
+            </View>
+            <View style={styles.filterContainer}>
+                <Text>Vegetarian</Text>
+                <Switch 
+                    thumbColor={Colors.accentColor}
+                    trackColor={{true: Colors.primaryColor}}
+                    value={isVegetarian} 
+                    onValueChange={newValue => setIsVegetarian(newValue)}
+                />
+            </View>
+            <View style={styles.filterContainer}>
+                <Text>Lactose-Free</Text>
+                <Switch 
+                    thumbColor={Colors.accentColor}
+                    trackColor={{true: Colors.primaryColor}}
+                    value={isLactoseFree} 
+                    onValueChange={newValue => setIsLactoseFree(newValue)}
+                />
+            </View>
         </View>
     );
 }
@@ -27,8 +69,20 @@ FiltersScreen.navigationOptions = (navData) => {
 const styles = StyleSheet.create({
     screen: {
         flex: 1, 
-        justifyContent: "center",
         alignItems: "center"
+    },
+    title: {
+        fontSize: 22,
+        fontWeight: "bold",
+        margin: 20,
+        textAlign: "center"
+    }, 
+    filterContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        width: "80%",
+        marginBottom: 30
     }
 })
 export default FiltersScreen;
